64cdaf6a6f3d63a02e44f090df09684db6acb92b
Hanief00 <hanifadaadaaja@gmail.com>
2025-06-10 23:16:31 +0700

    fix: erase tambah pengeluaran page

--- src/app/pengeluaran/tambah/page.tsx
+++
@@ -1,6 +0,0 @@
-// src/app/pengeluaran/tambah/page.tsx
-import TambahPengeluaran from '@/app/components/pengeluaran/TambahPengeluaran';
-
-export default function TambahPengeluaranPage() {
-  return <TambahPengeluaran />;
-}
